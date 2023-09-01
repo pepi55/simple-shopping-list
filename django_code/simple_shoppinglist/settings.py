42aa86a0f325a2d8da66aee9e484fb92937be42b
Pepi Dimi <pepi55@live.com>
2023-09-01 22:39:37 +0200

    v2.1.1 revert settings

--- django_code/simple_shoppinglist/settings.py
+++ django_code/simple_shoppinglist/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!+5a)ca2t%&hr!nvh284^e#_*&332htki#l9@k039$#1qb4)^x"
-#CSRF_COOKIE_SECURE = True
+SECRET_KEY = os.environ["SECRET_KEY"]
+CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "127.0.0.1", ".petar-dev.com", "pie3", "192.168.2.43", "84.82.138.101" ]
 
