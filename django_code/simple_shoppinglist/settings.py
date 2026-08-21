30355414b437d08fe95d5300239a863c24904cac
pepi55 <pepi55@live.com>
2026-08-22 00:13:50 +0200

    feat(settings): add CSRF_COOKIE_SECURE, SESSION_COOKIE_SECURE, SECURE_SSL_REDIRECT, HSTS, XSS, and clickjacking headers

--- django_code/simple_shoppinglist/settings.py
+++ django_code/simple_shoppinglist/settings.py
@@ -23,7 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
